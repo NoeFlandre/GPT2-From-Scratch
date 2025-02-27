992ae57301dade6d96a2384d4540a2b6708fcc5e
NoeFlandre <noeflandre@gmail.com>
2025-02-27 16:23:15 +0530

    Updated some comments in config.py

--- src/model/config.py
+++ src/model/config.py
@@ -5,9 +5,9 @@ from dataclasses import dataclass
 
 @dataclass
 class GPT2Config:
-    block_size: int = 1024 #context window
-    vocab_size: int = 50257 #GPT2 vocabulary size
-    n_layer : int = 12 #number of layers
-    n_head : int = 12 #number of heads
-    n_embd : int = 768 #embedding dimension
+    block_size: int = 1024 #maximum context length
+    vocab_size: int = 50257 #number of unique tokens in the GPT2 vocabulary
+    n_layer : int = 12 #number of transformer layers
+    n_head : int = 12 #number of attention heads within an attention layer
+    n_embd : int = 768 #dimensionality of token embeddings
 

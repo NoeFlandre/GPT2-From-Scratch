c266a3c34271ae6fd6e0b033cac8883e51d6c342
NoeFlandre <noeflandre@gmail.com>
2025-02-27 19:41:24 +0530

    Added a method to use weight decay in gpt2.py

--- src/model/gpt2.py
+++ src/model/gpt2.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 from model.block import Block
 from model.config import GPT2Config
 from torch.nn import functional as F
+import inspect
 
 class GPT2(nn.Module):
     def __init__(self, config): # call the constructor of nn.Module
@@ -62,3 +63,34 @@ class GPT2(nn.Module):
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
         return logits, loss
+
+
+    # weight decay is used to force the model to distribute the work to multiple parameters and not allow certain parameters to get extensively too important, it's a form of regularization.
+    # we only decay the embeddings and the matmul parameters, not the LayerNorms and Biases
+
+    def configure_optimizers(self, weight_decay, learning_rate, device):
+          # we are starting with all the parameters requiring gradient
+          para_dict = {pn: p for pn, p in self.named_parameters()}
+          para_dict = {pn: p for pn, p in para_dict.items() if p.requires_grad}
+
+          # we are creating optim groups. Only 2D parameters will be weight decayed
+          decay_params = [p for n, p in para_dict.items() if p.dim() >= 2]
+          nodecay_params = [p for n, p in para_dict.items() if p.dim() < 2]
+
+          optim_groups = [
+              {"params": decay_params, "weight_decay": weight_decay},
+              {"params": nodecay_params, "weight_decay": 0.0},
+          ]
+
+          num_decay_params = sum(p.numel() for p in decay_params)
+          num_nodecay_params = sum(p.numel() for p in nodecay_params)
+          print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+          print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+
+          # create the AdamW optimizer using the fused kernel if available. This allows to not go through all the parameters with a loop and update them but rather use a fused kernel for efficiency
+          fused_available= 'fused' in inspect.signature(torch.optim.AdamW).parameters
+          use_fused = fused_available and 'cuda' in device
+          print(f"using fused kernel: {use_fused}")
+
+          optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+          return optimizer
